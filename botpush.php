4899262fd652688a2412c4d8cafaa3b33a89616a
jeerawat permsup <p.jeerawat.th@live.com>
2018-05-16 20:55:55 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,12 +6,12 @@ $access_token = 'LtQ9cC/RPw+Lju3CjezNCqQscG10Ykl2zMLqmHVNIeYciYmjulShzIolUFtnQeY
 
 $channelSecret = '022a4f3b857ac6a2daa6cd26ec614bdd';
 
-$pushID = 'Udf7737e11025b4cc7688157c97625491';
+$pushID = 'Uff363ec46555a2a9b1607cbaab878e08';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ผมรักพี่คนเดียวน่ะครับพี่ฝ้าย ยอดดวงใจของผม');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบระบบ รายงาน 1:1 (บริษัท สาครเคเบิล)');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
