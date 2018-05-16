caa76cbb65bee83760a337786f9714e02173e1ba
jeerawat permsup <p.jeerawat.th@live.com>
2018-05-16 15:13:32 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,19 +1,17 @@
 <?php
 
-
-
 require "vendor/autoload.php";
 
 $access_token = 'LtQ9cC/RPw+Lju3CjezNCqQscG10Ykl2zMLqmHVNIeYciYmjulShzIolUFtnQeYyxW0NmtpGH2xP/UMkx4JNUkdzDhcQaOqHapy8yfAqCEncQoxKuSCZ2TcDM9oBjxNQsdrzjVKpdY/txklE9mdLiwdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '022a4f3b857ac6a2daa6cd26ec614bdd';
 
-$pushID = 'U53a0bd685f468b37ffbcd4d065afd78d';
+$pushID = 'Udf7737e11025b4cc7688157c97625491';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('พิมพ์มาดิ');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ผมรักพี่คนเดียวน่ะครับพี่ฝ้าย ยอดดวงใจของผม');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
